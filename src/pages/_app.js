7c7ea9c120ba68d491bcf80499c30eb8366ca6a4
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-25 11:51:49 +0700

    feat: Setup swr to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { SWRConfig } from "swr";
+import "../styles/globals.css";
+import fetcher from "../utils/fetcher";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 1000 * 60 * 15,
+        fetcher: fetcher,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
