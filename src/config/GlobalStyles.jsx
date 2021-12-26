d7015f8bf711ad2b885634f6ebc0766e41cdd10c
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-26 19:38:49 +0700

    feat: Add global style component

---
+++ src/config/GlobalStyles.jsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "@xstyled/styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap');
+
+  html, body {
+    font-family: 'Nunito', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
