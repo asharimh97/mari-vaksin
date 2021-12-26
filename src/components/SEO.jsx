f559e1d192d6e7c6e82e0e9ae6293462c20f30b5
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-26 19:38:17 +0700

    feat: Add SEO component

---
+++ src/components/SEO.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function SEO() {
+  return (
+    <Head>
+      <title>Mari Vaksin!</title>
+      <meta
+        name="description"
+        content="Website pengecekan ketersediaan vaksin Kota Semarang"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
+export default SEO;
