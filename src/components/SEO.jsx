8edb850c6f3116351298660a2fd2a0e224fe3303
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-31 09:48:30 +0700

    feat: Add meta tags for SEO

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -4,11 +4,27 @@ function SEO() {
   return (
     <Head>
       <title>Mari Vaksin!</title>
+      <meta name="title" content="Mari Vaksin!" />
       <meta
         name="description"
         content="Website pengecekan ketersediaan vaksin Kota Semarang"
       />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta name="og:type" content="website" />
+      <meta name="og:title" content="Mari Vaksin!" />
+      <meta name="og:url" content="https://mari-vaksin.vercel.app" />
+      <meta
+        name="og:description"
+        content="Website pengecekan ketersediaan vaksin Kota Semarang"
+      />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content="@asharimh97" />
+      <meta
+        name="twitter:description"
+        content="Website pengecekan ketersediaan Kota Semarang"
+      />
     </Head>
   );
 }
