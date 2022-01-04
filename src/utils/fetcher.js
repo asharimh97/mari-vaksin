8425ff74355f61262ecb99397bd8107741d2af24
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-01-04 22:15:37 +0700

    feat: Adjust fetcher to handle no success api fetch

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,3 +1,20 @@
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// Implementation from error handling
+// https://swr.vercel.app/docs/error-handling
+
+const fetcher = async (...args) => {
+  const res = await fetch(...args);
+
+  if (!res.ok) {
+    // throw error if not 2xx
+    const error = new Error("An Error occurred while fetching data");
+
+    error.info = await res.json();
+    error.status = res.status;
+
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default fetcher;
