fe3aab1514d786cca6bd8a63e5483901dc11c8ac
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-01-08 21:07:41 +0700

    feat: Update seo content

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,35 +1,41 @@
 import Head from "next/head";
 
 function SEO() {
+  const meta = {
+    title: "💉 Mari Vaksin!",
+    description: "Website pengecekan ketersediaan vaksin Kota Semarang",
+    color: "#FFF",
+    type: "website",
+    // TODO: Change it to env variable and import it
+    url: "https://mari-vaksin.vercel.app",
+  };
   return (
     <Head>
-      <title>Mari Vaksin!</title>
-      <meta name="title" content="Mari Vaksin!" />
-      <meta
-        name="description"
-        content="Website pengecekan ketersediaan vaksin Kota Semarang"
-      />
+      <title>{meta.title}</title>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content={meta.color} />
+
+      <meta name="title" content={meta.title} />
+      <meta name="description" content={meta.description} />
+
       <link rel="icon" href="/favicon.ico" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="/favicon.ico" />
 
-      <meta name="og:type" content="website" />
-      <meta name="og:title" content="Mari Vaksin!" />
-      <meta name="og:url" content="https://mari-vaksin.vercel.app" />
-      <meta
-        name="og:description"
-        content="Website pengecekan ketersediaan vaksin Kota Semarang"
-      />
+      <meta name="og:type" content={meta.type} />
+      <meta name="og:title" content={meta.title} />
+      <meta name="og:site_name" content={meta.title} />
+      <meta name="og:url" content={meta.url} />
+      <meta name="og:description" content={meta.description} />
       <meta name="og:image" href="/favicon.ico" />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content="@asharimh97" />
-      <meta name="twitter:title" content="Mari Vaksin!" />
-      <meta
-        name="twitter:description"
-        content="Website pengecekan ketersediaan Kota Semarang"
-      />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
       <meta name="twitter:image" href="/favicon.ico" />
+      <meta name="twitter:url" content={meta.url} />
     </Head>
   );
 }
