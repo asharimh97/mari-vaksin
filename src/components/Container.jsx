5e82557752bb3a0040247d8e2a5edf3a865fddbe
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-01-09 21:45:36 +0700

    feat: Add container component

---
+++ src/components/Container.jsx
@@ -0,0 +1,41 @@
+import { any } from "prop-types";
+
+function Container({ children }) {
+  return (
+    <section className="container">
+      {children}
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          margin: auto;
+          padding: 32px 16px;
+          width: 100%;
+        }
+        @media (min-width: 640px) {
+          .container {
+            padding: 32px;
+          }
+        }
+
+        @media (min-width: 768px) {
+          .container {
+            width: 640px;
+          }
+        }
+
+        @media (min-width: 1024px) {
+          .container {
+            width: 750px;
+          }
+        }
+      `}</style>
+    </section>
+  );
+}
+
+Container.propTypes = {
+  children: any,
+};
+
+export default Container;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,5 @@
+export { default as Container } from "./Container";
 export { default as SEO } from "./SEO";
 export { default as VaccineCard } from "./VaccineCard";
+export { default as VaccineDates } from "./VaccineDates";
 export { default as VaccineFilter } from "./VaccineFilter";
