d415a13143d2e394241d2f4f965096d234c18db5
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-01-09 21:45:54 +0700

    feat: Add API siaga corona

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,5 @@
 const VICTORI_URL = "http://victori.semarangkota.go.id";
+const SIAGA_CORONA_URL = "https://siagacorona.semarangkota.go.id/halaman";
 
 // eslint-disable-next-line import/prefer-default-export
-export { VICTORI_URL };
+export { VICTORI_URL, SIAGA_CORONA_URL };
