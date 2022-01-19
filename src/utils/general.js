ee0a887eafbbda8e573b4c9f9659d4b763a55931
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-01-19 21:26:01 +0700

    feat: Add helper to filter data

--- src/utils/general.js
+++ src/utils/general.js
@@ -4,4 +4,15 @@ const cleanString = (str) => {
     .replace(/[\r\n]+/g, " ");
 };
 
-export { cleanString };
+/**
+ * Return filtered data by key and parameter
+ * @param {Array} listData
+ * @param {string} key
+ * @param {any} parameter
+ * @returns
+ */
+const filterListData = (listData, key, parameter) => {
+  return listData.filter((item) => item[key] === parameter);
+};
+
+export { cleanString, filterListData };
